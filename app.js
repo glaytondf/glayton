6881d420cb8dbcb3fe746552d22f9a09fbd3cdd3
glaytondf <glaytondf@gmail.com>
2016-12-13 09:00:41 -0200

    Inclusão da entidade eleitor

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usuario = require('./routes/usuario');
 var perfil = require('./routes/perfil');
 var aluno = require('./routes/aluno');
 var carro = require('./routes/carro');
+var eleitor = require('./routes/eleitor');
 
 var mongoose = require('mongoose');
 
@@ -58,6 +59,7 @@ app.use('/usuario', usuario);
 app.use('/perfil', perfil);
 app.use('/aluno', aluno);
 app.use('/carro', carro);
+app.use('/eleitor', eleitor);
 
 
 
